4df2590248a4fba73cda1185b557726a16da2be4
rtteal <rtteal@gmail.com>
2015-04-16 20:37:24 -0700

    removing unnecessary file

--- proxy-server/test.js
+++
@@ -1,6 +0,0 @@
-let argv = require('yargs').argv
-let exec = require('child_process').exec
-console.log('hey: ' + argv.exec + " " + argv._)
-exec(argv.exec + " " + argv._).stdout.on('data', function(data) {
-    console.log('stdout: ' + data)
-})
